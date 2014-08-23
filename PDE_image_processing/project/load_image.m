a8903db99988c19fca9a623946401fa1221bd1f8
MI User <user@maths.ox.ac.uk>
2014-08-23 10:19:27 +0100

    added starter script to load image and add noise

---
+++ PDE_image_processing/project/load_image.m
@@ -0,0 +1,18 @@
+%% Load Image and add noise
+% Read image and convert to double
+u = double(imread('swan-512.jpg'));
+r = u(:,:,1);
+g = u(:,:,2);
+b = u(:,:,3);
+u = 0.299*r + 0.587*g + 0.144*b;
+
+%%
+% Add normal noise with mean 0 and variance 10
+u = u + 25.0*randn (size(u));
+
+%%
+% Display before image
+figure
+colormap gray
+imagesc(uint8(u));
+title('before');
