800fbc813b2483450bc5a27463ecf16128089448
MI User <user@maths.ox.ac.uk>
2014-08-23 10:20:36 +0100

    added starter script to load image and add noise

--- PDE_image_processing/project/load_image.m
+++ PDE_image_processing/project/load_image.m
@@ -7,7 +7,7 @@ b = u(:,:,3);
 u = 0.299*r + 0.587*g + 0.144*b;
 
 %%
-% Add normal noise with mean 0 and variance 10
+% Add normal noise with mean 0 and variance 25
 u = u + 25.0*randn (size(u));
 
 %%
